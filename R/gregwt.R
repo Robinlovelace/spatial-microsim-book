b8e1a05ec4a8ac72f03b0d1a84230b25618aafe8
Robinlovelace <rob00x@gmail.com>
2014-12-08 11:05:53 +0000

    separate book functions and msim ones

--- R/gregwt.R
+++ R/gregwt.R
@@ -1,5 +1,5 @@
 # Testing the GREGWT package
-install.packages("~/other-repos/mikrosim//GREGWT_1.4.tar.gz", repos=NULL)
+# install.packages("~/other-repos/mikrosim//GREGWT_1.4.tar.gz", repos=NULL) # download from https://github.com/emunozh/mikrosim/
 
 library(GREGWT)
 con_age <- read.csv("data/SimpleWorld/age.csv")
@@ -10,8 +10,23 @@ ind$age <- cut(ind$age, b = c(-Inf, 49, Inf), l = c("a0.49", "a.50."))
 
 cat_age <- model.matrix(~ ind$age - 1)
 cat_sex <- model.matrix(~ ind$sex -1)[, c(2,1)]
-  
-  
+ind_cat <- cbind(cat_age, cat_sex)
+colnames(ind_cat) <- gsub("ind\\$(...)", "", colnames(ind_cat))
+dx <- rep(1, nrow(ind_cat))
+Tx <- cbind(cat_age, cat_sex)
+
+res <- GREGWT(ind_cat[,1:3], dx, Tx = Tx[1,])
+fw <- res$Final.Weights
+
+# Estaban's code
+# load the functions
+library('GREGWT')
+
+## Data ######
+# Load the data from csv files stored under ./Data
+age = read.csv("data/SimpleWorld/age.csv")
+sex = read.csv("data/SimpleWorld/sex.csv")
+ind = read.csv("data/SimpleWorld/ind-full.csv")
 # Age
 a0.49 <- vector(length=dim(ind)[1])
 a0.49[ind$age<50] = 1
@@ -25,14 +40,12 @@ X <- data.frame(
   "a0.49" = a0.49,
   "a.50" = a.50,
   "m" = m)
-
 # Initial weights
 # Not such a good idea to start with a vector of ones as initial weights!
-age <- con_age
-sex <- con_sex
-
 dx <- vector(length=dim(X)[1]) + 1
+
 # prepare a data.frame to store the result
+fweights <- NULL
 Result = data.frame(
   area=vector(length=dim(age)[1]),
   income=vector(length=dim(age)[1]),
@@ -47,12 +60,14 @@ for(area in seq(dim(age)[1])){
   # Get new weights with GREGWT
   Weights.GREGWT = GREGWT(X, dx, Tx, bounds=c(0,Inf))
   cat("area:", area, "\n")
+  fw <- Weights.GREGWT$Final.Weights
   # Estimate income
+  fweights <- c(fweights, fw)
   sum.income <- sum(Weights.GREGWT$Final.Weights * ind$income)
   cap.income <- sum(Weights.GREGWT$Final.Weights * ind$income / sum(Weights.GREGWT$Final.Weights))
   Result[area,] <- c(area, sum.income, cap.income)}
-# view the results
-View(Result)
-  
 
+fweights <- matrix(fweights, nrow = nrow(ind))
 
+ind_agg <- t(apply(fweights, 2, function(x) colSums(x * ind_cat)))
+ind_agg
