933f6758a063e5cc01db7dfbd93eaa3cb6955835
robinlovelace <rob00x@gmail.com>
2016-11-07 10:34:26 +0000

    Add link to course home

--- notes/seville-notes.R
+++ notes/seville-notes.R
@@ -1,4 +1,6 @@
 # Welcome to the course's R notes
+# All course material found/linked to:
+# https://github.com/Robinlovelace/spatial-microsim-book
 # examples will go here
 
 # First challenge: get set-up on RStudio server
@@ -9,3 +11,19 @@
 library(tmap)
 tmap_mode("view")
 example(qtm)
+
+# downloading and unzipping data
+u = "https://github.com/Robinlovelace/vspd-base-shiny-data/archive/master.zip"
+download.file(u, destfile = "master.zip")
+unzip("master.zip")
+dir.create("data")
+f = list.files(path = "vspd-base-shiny-data-master/",
+               full.names = T)
+file.copy(f, "data")
+
+
+
+# spatial data with R
+
+
+
