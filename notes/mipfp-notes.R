19dfbfe4caf641e3a928a91c5031883c7db803e6
robinlovelace <rob00x@gmail.com>
2016-11-09 11:58:54 +0000

    Tidy up mipfp 'no data' code

--- notes/mipfp-notes.R
+++ notes/mipfp-notes.R
@@ -1,54 +1,37 @@
 # mipfp to do spatial microsimulation without input data
 
 global = read.delim("data/Belgium/BelgiqueConting.txt")
-c1 = read.delim("data/Belgium/ContrainteAge.txt")
-c2 = read.delim("data/Belgium/ContrainteDipl.txt")
-c3 = read.delim("data/Belgium/ContrainteStatut.txt")
-c4 = read.delim("data/Belgium/ContrainteGenre.txt")
+in_age = read.delim("data/Belgium/ContrainteAge.txt")
+in_dip = read.delim("data/Belgium/ContrainteDipl.txt")
+in_sta = read.delim("data/Belgium/ContrainteStatut.txt")
+in_sex = read.delim("data/Belgium/ContrainteGenre.txt")
 
 # for one zone
 global_cons = xtabs(Freq ~ gener + dipl + statut + sex, data = global)
 
 i = 1 # zone number
-uz = unique(c1$com)
+uz = unique(in_age$com)
 z = uz[i]
 z = "92094"
 # data preparation
-age = c1$COUNT[c1$com == z]
-edu = c2$COUNT[c2$com == z]
-ocu = c3$COUNT[c3$com == z]
-sex = c4$COUNT[c4$com == z]
+age = in_age$COUNT[in_age$com == z]
+edu = in_dip$COUNT[in_dip$com == z]
+ocu = in_sta$COUNT[in_sta$com == z]
+sex = in_sex$COUNT[in_sex$com == z]
 
-target = list(
-  age,
-  edu,
-  ocu,
-  sex
-  )
+target = list(age, edu, ocu, sex)
 descript = list(1, 2, 3, 4)
 
-res = mipfp::Ipfp(seed = global_cons,
-                  target.list = descript,
-                  target.data = target)
+res = mipfp::Ipfp(global_cons, descript, target)
 identical(dimnames(res$x.hat), dimnames(global_cons))
 expa = as.data.frame.table(res$x.hat)
 
-truncated = expa
-truncated$COUNT = floor(expa$Freq)
-p = expa$Freq - truncated$COUNT
-n_missing = sum(p)
-index = sample(1:nrow(truncated),
-               size = n_missing,
-               prob = p,
-               replace=FALSE)
-truncated$COUNT[index] = truncated$COUNT[index] + 1
+# Integerisation, see here for code:
+# https://github.com/Robinlovelace/spatial-microsim-book/blob/master/R/functions.R
+source("R/functions.R") # loads functions into memory
+expa$int = int_trs(expa$Freq)
 
-int_expand_vector <- function(x){
-  index <- 1:length(x)
-  rep(index, round(x))
-}
-
-exp_indices = int_expand_vector(truncated$COUNT)
+exp_indices = int_expand_vector(expa$int)
 synth = expa[exp_indices,]
 
 # for many zones
