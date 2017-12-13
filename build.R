b8e8d437abbffb3d458c44b87f40bb95258dfcad
Robin Lovelace <rob00x@gmail.com>
2017-12-13 11:07:09 +0000

    Modify build script (kept for historical value)

--- build.R
+++ build.R
@@ -8,53 +8,57 @@
 # Chapter summaries at outset?
 # Mention of collaborative project early on
 
+pkgs <- c("knitr", "rmarkdown", "png", "ggmap", "dplyr", "ipfp", "rgeos", "mipfp", "rgdal", "gridExtra", "maptools", "jpeg", "tmap", "tidyr", "mlogit", "simPop")
+wpacks <- pkgs %in% installed.packages()
+install.packages(pkgs[!wpacks])
+
 # file.copy(from = "~/Documents/smr.bib", to = "bibliography.bib", overwrite = T)
 
-# View the order chapters will be knitted (see R/book-functions.R)
-# chap_ord <- c(7,16,10,5,12,2,8,4,13,14,15,11,1,3,6,9)
-cfiles <- list.files(pattern = "*.Rmd$")
-# cfiles <- cfiles[chap_ord] # chapter order
-cfiles
-
-# Add book header
-book_header = readLines(textConnection('---
-title: "Spatial microsimulation with R"
-output:
-\ \ pdf_document:
-\ \ \ \ fig_caption: yes
-\ \ \ \ highlight: monochrome
-\ \ \ \ includes: null
-\ \ \ \ keep_tex: yes
-\ \ \ \ number_sections: yes
-\ \ \ \ toc: yes
-bibliography: bibliography.bib
-csl: elsevier-harvard.csl
-layout: default
----'))
-
-source("R/book-functions.R")
-# file.remove("book.Rmd")
-# Rmd_bind(book_header = book_header) 
-Rmd_bind_mod(book_header = book_header)
-
-# Packages needed to build the book
-# install.packages("knitr", "rmarkdown", "png", "ggmap", "dplyr", "ipfp") 
-library(knitr)
-library(rmarkdown)
-
-# Build the book:
-render("book.Rmd", output_format = "pdf_document")
+# # View the order chapters will be knitted (see R/book-functions.R)
+# # chap_ord <- c(7,16,10,5,12,2,8,4,13,14,15,11,1,3,6,9)
+# cfiles <- list.files(pattern = "*.Rmd$")
+# # cfiles <- cfiles[chap_ord] # chapter order
+# cfiles
+# 
+# # Add book header
+# book_header = readLines(textConnection('---
+# title: "Spatial microsimulation with R"
+# output:
+# \ \ pdf_document:
+# \ \ \ \ fig_caption: yes
+# \ \ \ \ highlight: monochrome
+# \ \ \ \ includes: null
+# \ \ \ \ keep_tex: yes
+# \ \ \ \ number_sections: yes
+# \ \ \ \ toc: yes
+# bibliography: bibliography.bib
+# csl: elsevier-harvard.csl
+# layout: default
+# ---'))
+# 
+# source("R/book-functions.R")
+# # file.remove("book.Rmd")
+# # Rmd_bind(book_header = book_header) 
+# Rmd_bind_mod(book_header = book_header)
+# 
+# # Packages needed to build the book
+# # install.packages("knitr", "rmarkdown", "png", "ggmap", "dplyr", "ipfp") 
+# library(knitr)
+# library(rmarkdown)
+# 
+# # Build the book:
+# render("book.Rmd", output_format = "pdf_document")
 
 
 # Build the CRC-formated version - requires local files
 # need to build the .tex manually for references to compile
-source("R/build-CRC-version.R")
-# Make latex-specific changes automated
-booktex <- readLines("spatial-microsim-book.tex")
-booktex[grep("\\{Glossary\\}", booktex)]
-booktex <- gsub(pattern = "chapter\\{Glossary\\}", "chapter*\\{Glossary\\}\n\\\\addcontentsline{toc}{chapter}{Glossary}
-", booktex)
-writeLines(booktex, "spatial-microsim-book.tex")
+# source("R/build-CRC-version.R")
+# # Make latex-specific changes automated
+# booktex <- readLines("spatial-microsim-book.tex")
+# booktex[grep("\\{Glossary\\}", booktex)]
+# booktex <- gsub(pattern = "chapter\\{Glossary\\}", "chapter*\\{Glossary\\}\n\\\\addcontentsline{toc}{chapter}{Glossary}
+# ", booktex)
+# writeLines(booktex, "spatial-microsim-book.tex")
 # in case index does not build - run again!
 # system("pdflatex --interaction=nonstopmode  spatial-microsim-book.tex")
 
@@ -64,13 +68,13 @@ writeLines(booktex, "spatial-microsim-book.tex")
 # file.remove("book.Rmd")
 
 # Remove latex-specific document links for website
-cfiles <- list.files("/tmp", pattern = "*.Rmd", full.names = T)
-for(i in cfiles){
-  text <- readLines(i)
-  sel <- grepl("\\(\\#", text)
-  text <- text[!sel]
-  writeLines(text, con = i)
-}
+# cfiles <- list.files("/tmp", pattern = "*.Rmd", full.names = T)
+# for(i in cfiles){
+#   text <- readLines(i)
+#   sel <- grepl("\\(\\#", text)
+#   text <- text[!sel]
+#   writeLines(text, con = i)
+# }
 
 # # # regex with R - convert book ready for regexxing
 # d <- readLines("introduction.Rmd")
